e446060185b04ee4189a46d71ebeb49460ccd620
pot3 <sr400.xj1jr@gmail.com>
2021-09-08 14:34:05 +0900

    履歴機能を追加

--- src/index.js
+++ src/index.js
@@ -48,12 +48,13 @@ class Game extends React.Component {
           squares: Array(9).fill(null),
         },
       ],
+      stepNumber: 0,
       xIsNext: true,
     }
   }
   handleClick(i) {
     // sliceにより配列のコピーを作成
-    const history = this.state.history
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
     const current = history[history.length - 1]
     const squares = current.squares.slice()
 
@@ -66,13 +67,31 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     })
   }
+
+  jumpTo(step){
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    })
+  }
+
   render() {
     const history = this.state.history
-    const current = history[history.length - 1]
+    const current = history[this.state.stepNumber]
     const winner = calculateWinner(current.squares)
+
+    const moves = history.map((step, move)=>{
+      const desc = move ? 'Go to move # ' + move : 'Go to game start'
+      return(
+        <li key={move}>
+          <button onClick={()=>this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    })
 
     let status
     if (winner) {
@@ -91,7 +110,7 @@ class Game extends React.Component {
         </div>
         <div className='game-info'>
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     )
